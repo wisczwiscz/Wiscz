9f8eac75492794d7305e2654e8302197a224fc4d
wisczwiscz <3325172851@qq.com>
2025-05-28 14:49:42 +0800

    添加文件

---
+++ frontend/src/views/1.vue
@@ -0,0 +1,21 @@
+<template>
+  <div style="margin: 0 auto; width:200px">
+    <table border="1">
+      <caption>学生信息表</caption>
+      <thead>
+        <tr>
+          <th>学号</th>
+          <th>姓名</th>
+          <th>年龄</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>2204241220</td>
+          <td>吴豪</td>
+          <td>20</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
